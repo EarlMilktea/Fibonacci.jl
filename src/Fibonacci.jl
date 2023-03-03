7107ae8ad0e53d88a84767e55a300dc384297855
Sora Shiratani <hclfuranpon_compotayamano_0117@outlook.jp>
2023-03-03 22:51:36 +0900

    First impl.

--- src/Fibonacci.jl
+++ src/Fibonacci.jl
@@ -1,5 +1,43 @@
 module Fibonacci
 
-# Write your package code here.
+export fibonacci
+
+"""
+    fibonacci(n)
+
+Compute the nth Fibonacci number, as BigInt.
+
+# Examples
+```jldoctest
+julia> using Fibonacci
+
+julia> fibonacci(1)
+1
+
+julia> fibonacci(2)
+1
+
+julia> fibonacci(3)
+2
+```
+"""
+function fibonacci(n::Integer)
+    v0, v1 = big(0), big(1)
+    if n < 0
+        for _ ∈ -1:-1:n
+            v0, v1 = v1 - v0, v0
+        end
+        v0
+    elseif n == 0
+        v0
+    elseif n == 1
+        v1
+    else
+        for _ ∈ 2:n
+            v1, v0 = v0 + v1, v1
+        end
+        v1
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using Fibonacci
+using Documenter
 using Test
 
+const TESTCASES = (
+    (-2, -1),
+    (-1, 1),
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (10, 55),
+    (40, 102334155),
+    (100, 354224848179261915075),
+)
+
 @testset "Fibonacci.jl" begin
-    # Write your tests here.
+    for (n, v) ∈ TESTCASES
+        @test fibonacci(n) == v
+    end
+    doctest(Fibonacci; manual=false)
 end
