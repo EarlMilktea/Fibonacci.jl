d6272bcc4267d671fa0b19abec571f2785cd0fba
Documenter.jl <documenter@juliadocs.github.io>
2023-03-03 13:51:08 +0000

    build based on a27cdc4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Fibonacci","category":"page"},{"location":"#Fibonacci","page":"Home","title":"Fibonacci","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Fibonacci.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Fibonacci]","category":"page"}]
+}
